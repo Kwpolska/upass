7543724aa639244e312cc7e6ce6bbdac160a562f
Chris Warrick <kwpolska@gmail.com>
2020-01-01 00:00:00 +0000

    2020! :fireworks:

--- docs/conf.py
+++ docs/conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'upass'
-copyright = u'2015-2019, Chris Warrick'
+copyright = u'2015-2020, Chris Warrick'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
